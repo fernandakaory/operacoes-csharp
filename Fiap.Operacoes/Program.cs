a6e9b801319d51debf1638a04da19968b8898c8e
fernandakaory <fernandakaory2@gmail.com>
2025-02-25 22:28:22 -0300

    criando diretório

--- Fiap.Operacoes/Program.cs
+++ Fiap.Operacoes/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,7 +49,15 @@ namespace Fiap.Operacoes
 
                 Console.WriteLine($"Ambos positivos {ambosPositivos}");
                 Console.WriteLine($"Nenhum número é negativo? {nenhumNegativo}");
-
+                
+                if(Directory.Exists("C:/arquivosProcessar"))
+                {
+                    Console.WriteLine("Pasta Existe");
+                } else
+                {
+                    Console.WriteLine("Pasta não existe");
+                    Directory.CreateDirectory("C:/arquivosProcessar");
+                }
 
 
                 Console.Write("Digite qualquer coisa para sair: ");
